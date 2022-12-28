8cc5436ddc4d0f1c989e157c746be4e6f143f769
wesleyara <wes1gom@gmail.com>
2022-12-28 13:15:07 -0300

    fix: change overflow

--- src/components/Steps/FirstStep.tsx
+++ src/components/Steps/FirstStep.tsx
@@ -5,15 +5,17 @@ export const FirstStep = () => {
   const dispatch = useDispatch();
 
   return (
-    <div className="fade-up mx-auto flex h-[100vh] w-[550px] flex-col items-center justify-center gap-5 text-center">
-      <h1 className="text-[40px]">Bem vindo ao Need Water!</h1>
-      <span>
-        Assim como o ar que respiramos, beber água bem faz toda a diferença para
-        a nossa saúde! Vamos começar a fazer isso com maestria?
-      </span>
-      <button className="btn" onClick={() => dispatch(next())}>
-        Vamos lá!
-      </button>
+    <div className="overflow-hidden">
+      <div className="fade-up mx-auto flex h-[100vh] w-[550px] flex-col items-center justify-center gap-5 text-center">
+        <h1 className="text-[40px]">Bem vindo ao Need Water!</h1>
+        <span>
+          Assim como o ar que respiramos, beber água bem faz toda a diferença
+          para a nossa saúde! Vamos começar a fazer isso com maestria?
+        </span>
+        <button className="btn" onClick={() => dispatch(next())}>
+          Vamos lá!
+        </button>
+      </div>
     </div>
   );
 };
